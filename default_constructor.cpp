1e76da552c7670e0c4db52d265dda501ac2f2b36
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:39:14 +0530

    Create default_constructor.cpp

---
+++ default_constructor.cpp
@@ -0,0 +1,31 @@
+//akarsh khot
+//23070123012
+//Aim - to learn and use the use of constructor statements using constructor.
+
+#include <iostream>
+using namespace std;
+
+class construct{
+    public:
+    int a,b;
+construct(){
+    a = 10;
+    b = 20;
+}
+void display(){
+cout << "a ="<< a <<endl<< "b ="<<b;
+}
+};
+
+int main() {
+    construct constructor;
+    constructor.display();    
+
+    return 0;
+}
+
+/*
+OUTPUT
+a =10
+b =20
+*/
