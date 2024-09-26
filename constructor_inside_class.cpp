0449afa7496f80f8124f6c674025ef7134ed96a3
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:38:02 +0530

    Create constructor_inside_class.cpp

---
+++ constructor_inside_class.cpp
@@ -0,0 +1,42 @@
+//akarsh khot
+//23070123012
+//Aim - to learn consturct inside class.
+
+#include<iostream>
+using namespace std;
+
+class date
+{
+    int d;
+    int m;
+    int y;
+
+
+    public:
+    date()
+    {
+        cout<<"Enter date: ";
+        cin>>d;
+        cout<<"Enter month: ";
+        cin>>m;
+        cout<<"Enter  Year: ";
+        cin>>y;
+        cout<<"Today's Date is: "<<endl;
+        cout<<d<<"/"<<m<<"/"<<y;
+    }
+};
+int main()
+{
+    date today;
+}
+
+
+
+/*
+OUTPUT
+Enter date: 5
+Enter month: 9
+Enter  Year: 24
+Today's Date is: 
+5/9/24
+*/
